640b687c477216c00886e5f29d9b6704ac21e1b0
Vicente Elias <75823840+Lemaziv@users.noreply.github.com>
2022-01-25 18:13:09 -0500

    Ejemplo Patron Builder

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Casa LaCasa = new Casa();
+        Casa3Paredes Constructor = new Casa3Paredes(LaCasa);
+        LaCasa.printHouse();
+    }
+}
