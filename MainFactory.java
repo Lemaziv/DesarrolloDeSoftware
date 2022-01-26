ae414ba49895e61d489e3eb9e4de42749e2929b0
Vicente Elias <75823840+Lemaziv@users.noreply.github.com>
2022-01-25 20:46:34 -0500

    Delete MainFactory.java

--- MainFactory.java
+++
@@ -1,8 +0,0 @@
-public class MainFactory {
-    public static void main(String[] args) {
-        Animal dog = new DogFactory().createAnimal();
-        dog.print();
-        Animal cat = new CatFactory().createAnimal();
-        cat.print();
-    }
-}
