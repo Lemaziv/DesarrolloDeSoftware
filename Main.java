16fb2e285e70e3960084a76f3c5befc2ba028dbc
Vicente Elias <75823840+Lemaziv@users.noreply.github.com>
2022-02-21 00:02:19 -0500

    Delete Main.java

--- Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        Casa LaCasa = new Casa();
-        Casa3Paredes Constructor = new Casa3Paredes(LaCasa);
-        LaCasa.printHouse();
-    }
-}
